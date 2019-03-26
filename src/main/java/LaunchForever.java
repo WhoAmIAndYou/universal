728a6b1c28f6b9af942f87d82a851d36911ea1c5
Gary Ma <jma@silver-peak.com>
2019-03-26 16:57:27 +0800

    first trace

---
+++ src/main/java/LaunchForever.java
@@ -0,0 +1,14 @@
+/**
+ * Created with Intellij IDEA.
+ * User: jma
+ * Date: 2019/3/26
+ * Time: 16:52
+ * To change this template use File | Settings | File and Code Templates
+ */
+public class LaunchForever {
+
+
+    public static void main(String [] args){
+
+    }
+}
